40d32f728b6a349b5d8bc81948299cc79b239ef9
Colin <61954544+DreamingCoders@users.noreply.github.com>
2020-12-15 15:41:12 -0500

    Update finalize.php

--- finalize.php
+++ finalize.php
@@ -1,5 +1,9 @@
 <?php
+include_once("avatar.php");
+
 echo"Your AvatarNetworkScript installation returned successful!";
-echo"Your data:";
-echo"Server name: Null";
-echo"Admin account: Null";
+echo"Your data:" . $obj . ".";
+echo"Server name: ".$serverName."";
+echo"Admin account: ".$adminUser."";
+
+echo"<a href="/">View your all new website!</a>";
